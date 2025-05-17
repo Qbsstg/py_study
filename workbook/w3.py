0f9c2dfad504ba929f82ca87b0fadee67039be8a
whitetower <angela-hxying@ 163.com>
2025-05-17 16:20:58 +0800

    给老公审核

--- workbook/w3.py
+++ workbook/w3.py
@@ -7,3 +7,14 @@
 # 现有新来了一辆车 'A0000', 请把它放在 car_nums 的最前面
 # 'A00X9' 这辆车现在不租你的停车位上，请把它从 car_nums 中删掉
 # 现在来了一个车队 ['B0001','B0002','B003'], 请用一行代码把它加到 car_nums 中
+if __name__ == '__main__':
+    car_nums = ['A0001', 'A00X9', 'A0027']
+    car_nums.insert(3, 'A0030')
+    print(car_nums)
+    car_nums.insert(0, 'A0000')
+    print(car_nums)
+    del car_nums[1]
+    print(car_nums)
+    car_nums1 = ['B0001','B0002','B003']
+    car_nums2 = car_nums + car_nums1
+    print(car_nums2)
